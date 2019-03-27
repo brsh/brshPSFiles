188e885a6fbd0e5c32e8cbd0094656093e61a002
Brian Sheaffer <brsh@users.noreply.github.com>
2019-03-27 10:41:40 -0700

    Adjusted license creation - mit needed var work

--- private/license.ps1
+++ private/license.ps1
@@ -13,15 +13,15 @@ function Get-LicenseText {
 #>
 	Switch ($License) {
 		'Apache2' {
-			"Copyright (c) $((Get-Date).Year) by $Author, under the Apache License, Version 2.0."
+			"Copyright (c) $((Get-Date).ToString('yyyy')) by $Author, under the Apache License, Version 2.0."
 			break
 		}
 		'GNUv3' {
-			"Copyright (c) $((Get-Date).Year) by $Author, under the GNUv3 license."
+			"Copyright (c) $((Get-Date).ToString('yyyy')) by $Author, under the GNUv3 license."
 			break
 		}
 		'MIT' {
-			"Copyright (c) $((Get-Date).Year) by $Author, under the MIT license."
+			"Copyright (c) $((Get-Date).ToString('yyyy')) by $Author, under the MIT license."
 			break
 		}
 		DEFAULT { 'To the extent within my power and possible under law, the author(s) have dedicated all copyright and related and neighboring rights to the public domain worldwide. This is distributed without any warranty.'; break }
@@ -36,18 +36,28 @@ function Set-LicenseFile {
 	)
 	if (test-path $root) {
 		try {
+			[bool] $FileCreated = $false
 			Switch ($License) {
 				'Apache2' {
 					Copy-Item $script:scriptpath\config\Apachev2.txt $root\license.txt
+					$FileCreated = $true
 					break
 				}
 				'GNUv3' {
 					Copy-Item $script:scriptpath\config\GNUv3.txt $root\license.txt
+					$FileCreated = $true
 					break
 				}
 				'MIT' {
-					Copy-Item $script:scriptpath\config\mit.txt $root\license.txt
+					$LicenseFile = (Get-Content $script:scriptpath\config\mit.txt).Replace('Copyright (c) [year] [fullname]', "Copyright (c) $((Get-Date).ToString('yyyy')) $Author")
+					$LicenseFile | Out-File -FilePath "$root\license.txt" -Encoding utf8 -NoClobber -ErrorAction Stop
+					$FileCreated = $true
 					break
+				}
+
+				DEFAULT {
+					Write-Status -Message 'No License specified - defaulting to Public Domain' -Type 'Warning' -Level 1
+					$FileCreated = $false
 				}
 			}
 			Write-Status -Message "License file created." -Type 'Good' -Level 1
